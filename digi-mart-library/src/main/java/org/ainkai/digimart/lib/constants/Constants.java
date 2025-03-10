936f4460536a5f8871109d3e1edffa87d0d2ab5c
Mitakshar15 <work.mitakshar@gmail.com>
2025-03-10 18:53:56 +0530

    Fixed dependency issue

---
+++ digi-mart-library/src/main/java/org/ainkai/digimart/lib/constants/Constants.java
@@ -0,0 +1,9 @@
+package org.ainkai.digimart.lib.constants;
+
+public class Constants {
+
+
+    public static final String DIGIMART_APP_ID = "digimart";
+    public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+
+}
